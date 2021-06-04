5d138260223b20c41d6763073228dcbe377dedbc
Sahin Yort <thesayyn@gmail.com>
2021-06-04 09:37:27 +0200

    feat: complete one of support (#53)

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,2 +1,2 @@
-export * from "./descriptor";
-export * from "./plugin";
+// we have this file to pass empty file logic in ts_project
+const MARKER = true;
--- test/oneof.spec.ts
+++ test/oneof.spec.ts
@@ -1,15 +1,31 @@
-import {OneOfWithoutAnyOtherFields} from "./oneof"; 
+import { OneOfWithoutAnyOtherFields, OneOf } from "./oneof";
 
 describe("oneof", () => {
 
+    it("should take last property  if two properties are present at the same time if given via constructor", () => {
+        const oneOf = new OneOfWithoutAnyOtherFields(
+            // @ts-expect-error
+            { age: "21", date_of_birth: "21.01.1999", nickname: "Heisenberg", realname: "Walter White" }
+        )
 
-    it("should set oneof field", () => {
-        const oneOf = new OneOfWithoutAnyOtherFields({
-            age: "21"
-        })
+        expect(oneOf.toObject()).toEqual({
+            date_of_birth: "21.01.1999",
+            realname: "Walter White"
+        });
+        expect(oneOf.age_or_dateofbirth).toBe("date_of_birth");
+        expect(oneOf.nickname_or_realname).toBe("realname");
+    });
 
-
-        expect(oneOf.toObject()).toEqual({age: "21"})
+    it("should take last seen if two properties are present at the same time", () => {
+        const oneOf = new OneOf({nickname: "thesayyn"});
+        oneOf.date_of_birth = "18.11.1998";
+        oneOf.age = "22";
+        expect(oneOf.toObject()).toEqual({
+            age: "22",
+            nickname: "thesayyn"
+        });
+        expect(oneOf.age_or_dateofbirth).toBe("age");
     })
+
 
 })
