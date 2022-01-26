388e4b8e55fcb84155b67c75ad2a0d88385fd3cc
Chris Kruining <c.kruining@fyn.nl>
2022-01-26 15:59:50 +0100

    refactor to snake case

--- test/no_namespace/rpc.spec.ts
+++ test/no_namespace/rpc.spec.ts
@@ -1,6 +1,6 @@
 describe('No namespace', () => {
     it('should export a class named `NoNamespace`', async () => {
-        const message = await import('./noNamespace.js');
+        const message = await import('./no_namespace.js');
 
         expect(message.NoNamespace.name).toBe('NoNamespace');
     });
