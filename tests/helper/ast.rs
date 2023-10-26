37bfda28c282afbf0794aa718e5719f605090a9a
thesayyn <thesayyn@gmail.com>
2023-10-25 21:06:54 -0700

    cleanup

--- tests/helper/ast.rs
+++ tests/helper/ast.rs
@@ -1,17 +1,20 @@
-use std::{path::Path};
 use swc_common::{
     errors::{ColorConfig, Handler},
     SourceMap,
-    sync::Lrc
+    sync::Lrc, FileName
 };
 use swc_ecma_parser::{lexer::Lexer, Capturing, Parser, StringInput, Syntax};
+use std::io::Read;
 
 fn main() {
+    println!("> ");
     let cm: Lrc<SourceMap> = Default::default();
     let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
-    let fm = cm
-        .load_file(Path::new("test.ts"))
-        .expect("failed to load test.ts");
+    let mut input =  Vec::new();
+    let stdin = std::io::stdin();
+    let mut handle = stdin.lock();
+    handle.read_to_end(&mut input).unwrap();
+    let fm = cm.new_source_file(FileName::Real("stdin.ts".into()), String::from_utf8(input).unwrap());
 
     let lexer = Lexer::new(
         Syntax::Typescript(Default::default()),
@@ -28,17 +31,8 @@ fn main() {
         e.into_diagnostic(&handler).emit();
     }
 
-    let _module = parser
-        .parse_typescript_module()
-        .map_err(|e| e.into_diagnostic(&handler).emit())
-        .expect("Failed to parse module.");
-
-    // let j = serde_json::to_string(&_module).expect("can not serialize");
-
-    // Print, write to a file, or send to an HTTP server.
-    println!("{:?}", _module);
-
-    // std::fs::write("output.json", &j).unwrap();
-
-    println!("Tokens: {:?}", parser.input().take());
+    println!("{:#?}", parser
+    .parse_typescript_module()
+    .map_err(|e| e.into_diagnostic(&handler).emit())
+    .expect("Failed to parse module.").body);
 }
